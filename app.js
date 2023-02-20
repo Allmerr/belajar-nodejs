ab7fe9072bdd7a637340a0107bc97887b7ea501e
Kevin Almer <kevinalmer4@gmail.com>
2023-02-20 20:39:49 +0700

    contact app full part

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
+const yargs = require("yargs");
 const contacts = require("./contacts");
 
-const main = async () => {
-    const nama = await contacts.buatPertanyaan("Masukan Nama Anda ? ");
-    const nomor = await contacts.buatPertanyaan("Masukan nomor Anda ? ");
-    const email = await contacts.buatPertanyaan("Masukan Email Anda ? ");
+yargs
+    .command({
+        command: "add",
+        describe: "Add New Contact",
+        builder: {
+            nama: {
+                describe: "Full Name",
+                demandOption: true,
+                type: "string",
+            },
+            noHp: {
+                describe: "Full Name",
+                demandOption: true,
+                type: "string",
+            },
+            email: {
+                describe: "Email Adress",
+                demandOption: false,
+                type: "string",
+            },
+        },
+        handler(argv) {
+            contacts.saveContact(argv.nama, argv.noHp, argv.email);
+        },
+    })
+    .demandCommand();
 
-    contacts.simpanContacts(nama, nomor, email);
-};
+yargs.command({
+    command: "list",
+    describe: "Get The All Contact List",
+    handler() {
+        contacts.listContact();
+    },
+});
 
-main();
+yargs.command({
+    command: "detail",
+    describe: "Get The Detail Contact by Nama",
+    builder: {
+        nama: {
+            describe: "Full Name",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        contacts.detailContact(argv.nama);
+    },
+});
+
+yargs.command({
+    command: "delete",
+    describe: "Delete The Spesific Given Contact",
+    builder: {
+        nama: {
+            describe: "Full Name",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        contacts.deleteContact(argv.nama);
+    },
+});
+
+yargs.parse();
--- contacts.js
+++ contacts.js
@@ -1,47 +1,81 @@
-// import core modules
-
+const chalk = require("chalk");
 const fs = require("fs");
-const readline = require("readline");
+const validator = require("validator");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-// cek ketersedian folder
-const pathFolder = "./data";
-if (!fs.existsSync(pathFolder)) {
-    fs.mkdir(pathFolder, (e) => this);
+const folderPath = "./data";
+if (!fs.existsSync(folderPath)) {
+    fs.mkdir(folderPath);
 }
 
-// cek ketersedian file
-const pathFile = "./data/contacts.json";
-if (!fs.existsSync(pathFile)) {
-    fs.writeFileSync(pathFile, "[]", "utf-8");
+const filePath = "./data/contacts.json";
+if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
-const buatPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, rejects) => {
-        return rl.question(pertanyaan, (hasilJawabanPertanyaan) => {
-            resolve(hasilJawabanPertanyaan);
-        });
+const getDataContacts = () => {
+    const data = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(data);
+};
+
+const saveContact = (nama, noHp, email) => {
+    const data = { nama, email, noHp };
+    const contacts = getDataContacts();
+
+    const isDuplicatedName = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+    if (isDuplicatedName) {
+        return console.log(chalk.bgRed("Name Has Been Taken, Add New Contact Has Been Failed!"));
+    }
+
+    if (!validator.isMobilePhone(noHp, "id-ID")) {
+        return console.log(chalk.bgRed("Your Nomor Phone Incorect, Add New Contact Has Been Failed!"));
+    }
+
+    contacts.push(data);
+    fs.writeFileSync(filePath, JSON.stringify(contacts));
+    return console.log(chalk.bgGreen("Add New Contact Has Done Successful!"));
+};
+
+const listContact = () => {
+    const contacts = getDataContacts();
+    console.log(chalk.bgGreen("All List Contact"));
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHp}`);
     });
 };
 
-const simpanContacts = (nama, nomor, email) => {
-    const ourNewMember = { nama, nomor, email };
-
-    let data = fs.readFileSync("data/contacts.json", "utf-8");
-    data = JSON.parse(data);
-    data.push(ourNewMember);
-    data = JSON.stringify(data);
-
-    fs.writeFileSync("./data/contacts.json", data, (e) => {
-        console.log(e);
+const detailContact = (nama) => {
+    const contacts = getDataContacts();
+    const isGet = contacts.find((contact) => {
+        return contact.nama.toLowerCase() === nama.toLowerCase();
     });
-
-    console.log(`Terimakasih ${nama}, Nomor Hp Anda Sudah Terdaftar`);
-    rl.close();
+    if (isGet) {
+        console.log(chalk.bgGreen(`Name ${nama} is Found, Get Detail Contact Successful!`));
+        if (isGet.email) {
+            return console.log(`${isGet.nama} - ${isGet.noHp} - ${isGet.email}`);
+        } else {
+            return console.log(`${isGet.nama} - ${isGet.noHp}`);
+        }
+    } else {
+        return console.log(chalk.bgRed(`Name ${nama} is not Found, Get Detail Contact Failed!`));
+    }
 };
 
-module.exports = { buatPertanyaan, simpanContacts };
+const deleteContact = (nama) => {
+    const contacts = getDataContacts();
+    const newContacts = [];
+    contacts.forEach((contact) => {
+        if (contact.nama.toLowerCase() !== nama.toLowerCase()) {
+            newContacts.push(contact);
+        }
+    });
+
+    if (contacts.length === newContacts.length) {
+        return console.log(chalk.bgRed(`Name ${nama} is not Found, Delete Spesific Contact Failed!`));
+    }
+
+    fs.writeFileSync(filePath, JSON.stringify(newContacts));
+
+    console.log(chalk.bgGreen(`Name ${nama} is Found, Contact  Has Been DeleteSuccessful!`));
+};
+
+module.exports = { saveContact, listContact, detailContact, deleteContact };
