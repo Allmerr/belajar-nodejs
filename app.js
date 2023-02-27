ee4d3aa74cb4d1d3d10c2ba6715a0cb21c8fd999
Kevin Almer <kevinalmer4@gmail.com>
2023-02-27 16:31:53 +0700

    tambah ejs

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
+// @ts-check
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// gunakan ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.sendFile("./views/index.html", { root: __dirname });
+    // res.sendFile("./views/index.html", { root: __dirname });
+    const mahasiswa = [
+        {
+            nama: "kevin almer",
+            email: "kevinalmer4@gmail.com",
+        },
+        {
+            nama: "Kerin Dwi Almira",
+            email: "kerindwi@gmail.com",
+        },
+    ];
+    res.render("index", {
+        nama: "Kevin Almer",
+        title: "Home",
+        mahasiswa,
+    });
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", { root: __dirname });
+    res.render("about", {
+        title: "About",
+    });
 });
 
-app.get("/contact", (req, res) => {
-    res.sendFile("./views/contact.html", { root: __dirname });
+app.get("/product/:id/kategori/:kategori", (req, res) => {
+    res.send(`Product nomor ${req.params.id} dengan kategori ${req.params.kategori} tersedia`);
 });
 
-app.use("/", (req, res) => {
+app.use((req, res) => {
+    res.header(404);
     res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.listen(port, () => {
-    console.log("Listening to http://localhost:3000");
+    console.log(`Listening to http://localhost:${port}`);
 });
